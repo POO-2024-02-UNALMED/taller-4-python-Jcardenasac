e90f5cbe28bb9ad1dc30b115cf4de885340b05b6
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-25 19:18:10 -0500

    b

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,10 +1,10 @@
 from classroom.asignatura import Asignatura
 
-class Grupo:
-    grado = "Grado 12"  # Valor por defecto
-    
 
-    def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+class Grupo:
+    grado = "Grado 12"
+
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas is not None else []
         self.listadoAlumnos = estudiantes if estudiantes is not None else []
@@ -16,12 +16,13 @@ class Grupo:
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
             lista = []
-        lista.append(alumno)
-        self.listadoAlumnos += lista
+        listaI = []
+        listaI.append(alumno)
+        self.listadoAlumnos += lista + listaI
 
-    def _str_(self):
-        return f"Grupo de estudiantes: {self._grupo}"
+    def __str__(self):
+        return "Grupo de estudiantes: " + self._grupo
 
-    @classmethod
+    @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
