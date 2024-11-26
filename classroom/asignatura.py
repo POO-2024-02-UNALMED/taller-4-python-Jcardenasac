1a031132898d3edd2bd6039e16518fee2c246a8b
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-25 19:02:13 -0500

    a

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
+    def _init_(self, nombre, salon="remoto"):
+        self.nombre = nombre
+        self.salon = salon
 
-    # def __str__(self):
-    #     pass
+    def _str_(self):
+        return f"{self.nombre} {self.salon}"
